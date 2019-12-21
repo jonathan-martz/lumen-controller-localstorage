8de6726c8d6d09b7aca60835179962837347728b
jonathan-martz <github@jmartz.de>
2019-12-21 16:49:34 +0100

    fix migrations

--- migrations/2019_12_20_1337_create_localstorage_table.php
+++ migrations/2019_12_20_1337_create_localstorage_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCookiesTable extends Migration
+class CreateLocalstorageTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCookiesTable extends Migration
      */
     public function up()
     {
-        Schema::create('cookies', function (Blueprint $table) {
+        Schema::create('local-storage', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('description');
@@ -27,6 +27,6 @@ class CreateCookiesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cookies');
+        Schema::dropIfExists('local-storage');
     }
 }
