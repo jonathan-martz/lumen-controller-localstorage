15772913811450d579694fdcd61d0c9ad5f86763
jonathan-martz <github@jmartz.de>
2019-12-21 16:06:05 +0100

    initial commit

---
+++ controller/LocalStorageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \http\Env\Response;
+use Illuminate\Http\JsonResponse;
+use \Illuminate\Http\Request;
+use \Illuminate\Support\Facades\DB;
+use \Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+/**
+ * Class LocalStorageController
+ * @package App\Http\Controllers
+ */
+class LocalStorageController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws ValidationException
+     */
+    public function select(Request $request)
+    {
+        $validation = $this->validate($request, [
+
+        ]);
+
+        $cookies = DB::table('local-storage')->get();
+
+        $this->addResult('local-storage',$cookies);
+        $this->addMessage('success', 'Local Storage loaded.');
+
+        return $this->getResponse();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,3 @@
+<?php
+
+Route::get('/local-storage', 'LocalStorageController@select');
